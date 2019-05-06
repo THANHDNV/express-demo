e023aa36bf7a113b202b37aa70216eccbb670744
THANHDNV <vanthanh971125@gmail.com>
2019-05-06 16:21:07 +0700

    save leads on leading page

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,8 +1,13 @@
+const models = require('../database/models')
+
 exports.get_landing = function(req, res, next) {
     res.render('landing', { title: 'Express' });
 }
 
 exports.submit_lead = function(req, res, next) {
-    console.log('Lead email: ', req.body.lead_email)
-    res.redirect('/')
+    return models.Lead.create({
+        email: req.body.lead_email
+    }).then(lead => {
+        res.redirect('/')
+    })
 }
