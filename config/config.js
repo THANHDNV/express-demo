c31e41593f7b23e885daa6b9ad55e0b5bdcee050
THANHDNV <vanthanh971125@gmail.com>
2019-05-21 09:36:33 +0700

    add google authentication

--- config/config.js
+++ config/config.js
@@ -1,5 +1,8 @@
 module.exports = {
     "facebook_api_key": "2131302723829943",
     "facebook_api_secret": "39e1f01a9f5d1d69727bcdd92453bad1",
-    "facebook_callback_url": "http://localhost:3000/facebook/callback"
+    "facebook_callback_url": "http://localhost:3000/facebook/callback",
+    "google_client_id": '624332213072-0ls9agj83hf8dtjr1cvoa45p8r4mgdfc.apps.googleusercontent.com',
+    'google_secret': 'gYNqWjDZ-x75d3rH7ohbwGwt',
+    'google_callback': "http://localhost:3000/google/callback"
 }
