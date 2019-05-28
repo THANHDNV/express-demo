58ae2ece0de326a6805ee2048ece9f4a200a08f9
THANHDNV <vanthanh971125@gmail.com>
2019-05-28 14:07:21 +0700

    fix controllers for production

--- controllers/fb.js
+++ controllers/fb.js
@@ -3,7 +3,9 @@ require('../passport_setup')(passport)
 
 module.exports = {
     login: function(req, res, next)  {
-        passport.authenticate('facebook', {scope:['email']})(req, res, next)
+        passport.authenticate('facebook', {scope:['email']}, (error,user) => {
+            req.redirect('/fb/callback')
+        })(req, res, next)
     },
 
     callback: function(req, res, next) {
@@ -11,17 +13,6 @@ module.exports = {
             successRedirect:'/',
             failureRedirect:'/login',
             scope:['email']
-        }, (error, user) => {
-            if (error) {
-                res.redirect('/login')
-            } else {
-                const token = jwt.sign({
-                    username: user.email
-                })
-                res.cookie('token',token, {
-                    maxAge: 86400 * 2
-                }).redirect('/')
-            }
         })(req,res,next)
     }
 }
--- controllers/google.js
+++ controllers/google.js
@@ -5,24 +5,15 @@ module.exports = {
     login: (req,res,next) => {
         return passport.authenticate('google', {
             scope: ['profile', 'email']
+        }, (error, user) => {
+            res.redirect('/google/callback')
         })(req,res,next)
     },
     callback: (req,res,next) => {
         return passport.authenticate('google', {
-            // successRedirect:'/',
-            // failureRedirect:'/login',
+            successRedirect:'/',
+            failureRedirect:'/login',
             scope: ['profile', 'email']
-        }, (error, user) => {
-            if (error) {
-                res.redirect('/login')
-            } else {
-                const token = jwt.sign({
-                    username: user.email
-                })
-                res.cookie('token',token, {
-                    maxAge: 86400 * 2
-                }).redirect('/')
-            }
         })(req,res,next)
     }
 }
